9bf2424af1da97ee11809ba8e994ff4e9739d41c
Demonbane18 <butterncreamz@gmail.com>
2023-09-22 16:52:53 +0800

    tried nonce again

--- next-helmlock/middleware.js
+++ next-helmlock/middleware.js
@@ -2,7 +2,34 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-  const res = NextResponse.next();
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
+  const requestHeaders = new Headers();
+  requestHeaders.set('x-nonce', nonce);
+  requestHeaders.set(
+    'Content-Security-Policy',
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, ' ').trim()
+  );
+  const res = NextResponse.next({
+    headers: requestHeaders,
+    req: {
+      headers: requestHeaders,
+    },
+  });
 
   // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient({ req, res });
